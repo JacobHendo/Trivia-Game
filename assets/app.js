d9b61a84d042543eeab0bcb0f26354baba10d04f
Jacob Henderson <Hendersonjacob1492@gmail.com>
2018-07-26 18:33:36 -0400

    added new buttons to the HTML and a new timer. Jumbotron for the Game title and info.

--- assets/app.js
+++ assets/app.js
@@ -1,59 +1,100 @@
 //window.onload = function() {
-  //  $("#stop").on("click", stopwatch.stop);
-    //$("#reset").on("click", stopwatch.reset);
-    //$("#start").on("click", stopwatch.start);
-  //};
-  
- 
-  //timeConverter: function(t) {
-
-   // var minutes = Math.floor(t / 60);
-    //var seconds = t - (minutes * 60);
-
-   // if (seconds < 10) {
-    //  seconds = "0" + seconds;
-    //}
-
-    //if (minutes === 0) {
-    //  minutes = "00";
-    //}
-    //else if (minutes < 10) {
-    //  minutes = "0" + minutes;
-   // }
+ $("#stop").on("click", stopwatch.stop);
+//$("#reset").on("click", stopwatch.reset);
+//$("#start").on("click", stopwatch.start);
+//};
 
 
+//timeConverter: function(t) {
+
+// var minutes = Math.floor(t / 60);
+//var seconds = t - (minutes * 60);
+
+// if (seconds < 10) {
+//  seconds = "0" + seconds;
+//}
+
+//if (minutes === 0) {
+//  minutes = "00";
+//}
+//else if (minutes < 10) {
+//  minutes = "0" + minutes;
+// }
 
 
-function gameResults(){
+
+
+function gameResults() {
     var answer1 = $("input[name=q-1]:checked").val();
     var answer2 = $("input[name=q-2]:checked").val();
     var answer3 = $("input[name=q-3]:checked").val();
     var answer4 = $("input[name=q-4]:checked").val();
     console.log("answer");
-   // results for question 1
-    if("correct" === answer1){
+    // results for question 1
+    if ("correct" === answer1) {
         correctAnswers++;
         console.log("#1 is correct");
     }
-    else if("incorrect" === answer1){
+    else if ("incorrect" === answer1) {
         incorrectAnswers++;
         console.log("#1 is incorrect");
     }
-    else if (undefined === answer1){
+    else if (undefined === answer1) {
         unAnswered++;
         console.log("#1 is unanswered");
     }
     // results for question 2
-    if("correct" === answer2){
+    if ("correct" === answer2) {
         correctAnswers++;
         console.log("#2 is correct");
     }
-    else if("incorrect" === answer2){
+    else if ("incorrect" === answer2) {
         incorrectAnswers++;
         console.log("#2 is incorrect");
     }
-    else if(undefined === answer2){
+    else if (undefined === answer2) {
         unAnswered++;
         console.log("#2 is unanswered");
     }
+
+     // results for question 3
+     if ("correct" === answer2) {
+        correctAnswers++;
+        console.log("#2 is correct");
+    }
+    else if ("incorrect" === answer2) {
+        incorrectAnswers++;
+        console.log("#2 is incorrect");
+    }
+    else if (undefined === answer2) {
+        unAnswered++;
+        console.log("#2 is unanswered");
+    }
+        // results for question 4
+    if ("correct" === answer2) {
+        correctAnswers++;
+        console.log("#2 is correct");
+    }
+    else if ("incorrect" === answer2) {
+        incorrectAnswers++;
+        console.log("#2 is incorrect");
+    }
+    else if (undefined === answer2) {
+        unAnswered++;
+        console.log("#2 is unanswered");
+    } 
+        // results for question 5
+    if ("correct" === answer2) {
+        correctAnswers++;
+        console.log("#2 is correct");
+    }
+    else if ("incorrect" === answer2) {
+        incorrectAnswers++;
+        console.log("#2 is incorrect");
+    }
+    else if (undefined === answer2) {
+        unAnswered++;
+        console.log("#2 is unanswered");
+    }
+
 }
